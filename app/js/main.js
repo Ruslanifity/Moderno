2432592afa963debf21c6db16ddcaae67c42aafc
ruslanifity <ruslanifity@gmail.com>
2020-02-13 13:11:48 +0200

    added section product-slider fix section product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
-$(function(){
+$(function () {
 
     var mixer = mixitup('.products__inner-box');
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+    });
+
+    $('.product-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
 
 });
